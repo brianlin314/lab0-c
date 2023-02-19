53b8bbeef40fd9914e73249e93bd7bddbfce9f26
brian.lin <brian0314b@gmail.com>
2023-02-19 14:20:00 +0800

    Implement q_size()
    Use list_for_each_safe to walk through the entire list

--- queue.c
+++ queue.c
@@ -115,7 +115,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head)) {
+        return 0;
+    }
+    int cnt = 0;
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        cnt++;
+    }
+    return cnt;
 }
 
 /* Delete the middle node in queue */
