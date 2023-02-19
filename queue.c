f016fe6b7a052cbae425c22a111c700d3c9142ee
brian.lin <brian0314b@gmail.com>
2023-02-19 13:55:31 +0800

    Implement q_new() and q_free
    q_new(): need to check NULL
    q_free(): used list_for_entry_safe and q_release_element

--- queue.c
+++ queue.c
@@ -14,11 +14,27 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head) {
+        return NULL;
+    }
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l) {
+        return;
+    }
+    element_t *cur, *tmp;
+    list_for_each_entry_safe (cur, tmp, l, list) {
+        q_release_element(cur);
+    }
+    free(l);
+    return;
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
